447a7fbb13934b1d234bd254c15866677fcf75f0
shossain <shossain@vws51.vector.local>
2020-05-15 20:06:10 -0400

    fixed bugs

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import concurrent.futures 
 import sys, csv
 
-NUM_CORES = 10
+NUM_CORES = 20
 
 class epsilon_greedy:
     def __init__(self, bandit_instance, c, num_sims=100, T=1000):
@@ -20,8 +20,9 @@ class epsilon_greedy:
         self.eps_t = np.zeros(T)
         self.n, self.k = bandit_instance.n, bandit_instance.k
         print("Optimal NSW: ", self.opt_NSW)
-   
-        self.empirical_rewards = np.ones((num_sims,self.n,self.k))
+        print("Optimal p:", self.opt_p)
+
+        self.empirical_rewards = np.ones((num_sims,self.n,self.k)) * 0.0001
         self.num_samples = np.zeros((num_sims, self.k))
         self.last_p = np.random.uniform(size=(num_sims, self.k))
         for sim in range(self.num_sims):
@@ -39,14 +40,12 @@ class epsilon_greedy:
         else:                   # exploit
             arm, rewards = self.bandit_instance.get_sample_p(last_p)
             nsw = self.bandit_instance.get_nsw(last_p)
+
         regret = self.opt_NSW - nsw
-        
         empirical_rewards[:,arm] = \
             (empirical_rewards[:,arm]*num_samples[arm] + rewards)/(num_samples[arm]+1)
-        num_samples[arm] += 1
-        
         best_p = solve_cvx(empirical_rewards, k, n)
-        return sim_id, best_p, regret
+        return sim_id, arm, rewards, best_p, regret
 
 
     def run(self):
@@ -78,13 +77,17 @@ class epsilon_greedy:
             for future in futures:
                 assert(future.done())
                 out_tup = future.result()
-                sim_id, best_p, regret = out_tup
+                sim_id, arm, rewards, best_p, regret = out_tup
+                self.empirical_rewards[sim_id,:,arm] = \
+                    (self.empirical_rewards[sim_id,:,arm]*self.num_samples[sim_id,arm] + rewards)/(self.num_samples[sim_id,arm]+1)
+                self.num_samples[sim_id,arm] += 1
+        
                 if best_p is not None:
                     self.last_p[sim_id] = best_p
                     t_regrets.append(regret)
                 else:
                     print("CVX failed (OPT p not found) on sim ", sim_id)
-            
+
             mean_regret = np.mean(t_regrets)
             std_regret = np.sqrt(np.var(t_regrets))
             self.mean_regrets[t] = mean_regret
@@ -93,9 +96,9 @@ class epsilon_greedy:
         return self.eps_t, self.explore_ratio, self.mean_regrets, self.std_regrets
                 
 def main():
-    c, T = 0.1, 1000
+    c, T = 0.15, 500
     bandit_instance = NSW_Bandit(6, 1)
-    eps_greedy = epsilon_greedy(bandit_instance, c, T=T)
+    eps_greedy = epsilon_greedy(bandit_instance, c, T=T, num_sims=20)
     eps_t, explore_ratio, mean_regrets, std_regrets = eps_greedy.run()
     cumulative_regrets = []
 
@@ -108,7 +111,7 @@ def main():
                 + [str(mean_regrets[t])] + [str(np.sum(mean_regrets[1:t+1]))] + [str(std_regrets[t])]
         csv_writer.writerow(row)
         csv_file.flush()
-       
+    print(mean_regrets) 
     #plt.plot(cumulative_regrets)
     #plt.imsave("cum_regrets")
     plt.plot(mean_regrets[1:])
